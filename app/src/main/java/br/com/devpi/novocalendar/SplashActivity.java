0efdcdf48dacac24d922ce05b6dadf0085f3658d
esdras.silva <eps.94@hotmail.com>
2017-07-01 13:00:10 -0300

    adicionando tela de abertura

---
+++ app/src/main/java/br/com/devpi/novocalendar/SplashActivity.java
@@ -0,0 +1,31 @@
+package br.com.devpi.novocalendar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    private static final int SPLASH_TIME_OUT =  2300;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Exibindo splash com um timer.
+             */
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
